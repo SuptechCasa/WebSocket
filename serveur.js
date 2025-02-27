08a0ce3ab6657c9b1163e41360540350e35bb6ed
enaanai <aenaanai@uae.ac.ma>
2025-02-27 09:42:56 +0000

    Initial commit

---
+++ serveur.js
@@ -0,0 +1,18 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('connection', (socket) => {
+    console.log('Nouveau client connecté');
+
+    socket.on('message', (message) => {
+        console.log('Message reçu:', message);
+        socket.send('Message bien reçu!');
+    });
+
+    socket.on('close', () => {
+        console.log('Client déconnecté');
+    });
+});
+
+console.log('Serveur WebSocket en écoute sur ws://localhost:8080');
